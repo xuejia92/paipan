7a86f28aa5cee00ab524c134e52db9a8fe16e930
dusy <1922884595@qq.com>
2022-02-20 23:58:00 +0800

    qingfeng

--- set.js
+++ set.js
@@ -1844,19 +1844,5 @@ var curShenSha ={
     return false;
   });
 
-  if (isSafari()) {
-      $(window).bind("pageshow", function (event) {
-          if (event.originalEvent.persisted && $('body').hasClass("no-cache")) {
-              document.body.style.display = "none";
-              window.location.reload();
-          }
-      });
-  }
-
-  function isSafari() {
-      if (navigator.userAgent.indexOf("Safari") > -1) {
-          return true;
-      }
-      return false;
-  } 
+  
 });
